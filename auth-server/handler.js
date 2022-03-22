7a6e047f77eac02e9c7790538ec4b7282112f631
jettb2 <jettblaschka@gmail.com>
2022-03-21 21:48:43 -0400

    updated

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,9 +20,10 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://jettb2.github.io/meet/"],
-  javascript_origins: ["https://jettb2.io", "http://localhost:3000"],
+  javascript_origins: ["https://jettb2.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
@@ -49,6 +50,51 @@ module.exports.getAuthURL = async () => {
     scope: SCOPES,
   });
 
+  module.exports.getAccessToken = event => {
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    //decode authorization code from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+    return new Promise((resolve, reject) => {
+
+      /**
+       * exchange authorization code for acess token with a "callback" after the exchange
+       * the callback in this case is an arrow function with the results as paramters: "err" and "token"
+       */
+
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+      .then((token) => {
+        //respond with OAuth token
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify(token),
+        };
+      })
+      .catch((err) => {
+        console.error(err);
+        return {
+          statusCode: 500,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify(err),
+        };
+      });
+  };
+
   return {
     statusCode: 200,
     headers: {
@@ -60,3 +106,53 @@ module.exports.getAuthURL = async () => {
   };
 };
 
+module.exports.getCalendarEvents = event => {
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  const access_token = decodeURIComponent(`${event.pathParameters.acess_token}`);
+
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+
+  })
+    .then(results => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allowed-Origin': '*'
+        },
+        body: JSON.stringify({ events: results.data.items })
+      };
+    })
+    .catch(error => {
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allowed-Origin': '*'
+        },
+        body: JSON.stringify(error)
+      }
+    })
+} 
